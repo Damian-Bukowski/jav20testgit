339cf487b9e9a1d6a381c0941f589662df3b6571
Damian Bukowski <damianbukowski@email.com>
2019-09-22 11:18:42 +0200

    zadanko z figurami

---
+++ src/Figura2.java
@@ -0,0 +1,12 @@
+public class Figura2 {
+    public static void main(String[] args) {
+
+        for (int i = 1; i < 7; i++) {
+            String empty = "";
+            for (int j = 1; j <= i; j++)
+                System.out.print(empty + "*");
+            System.out.println();
+        }
+    }
+}
+
---
+++ src/Figura3.java
@@ -0,0 +1,16 @@
+public class Figura3 {
+    public static void main(String[] args) {
+
+        String a = "******";
+        String b = "*    *";
+        int m = 5;
+
+        System.out.println(a);
+
+        for (int i = 0; i < m - 1; ++i) {
+            System.out.println(b);
+        }
+
+        System.out.println(a);
+    }
+}
